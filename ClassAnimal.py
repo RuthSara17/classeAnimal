85f19075d161e9ec4cbbacf62391867c62d5ac0e
Paola Medina <paola.medina.medina.17@gmail.com>
2025-02-24 17:33:44 -0400

    Manejo de parametros
    Sobrecarga simulada el metodo comer,
    manejo de args para manejo de varios argumentos

--- ClassAnimal.py
+++ ClassAnimal.py
@@ -5,31 +5,44 @@ class Animal:
         self.color = color
         self.genero = genero
 
-    # Método para mostrar la información del auto
+    # Método para mostrar la información del animal
     def mostrar_info(self):
         print(f"Especie: {self.especie}")
         print(f"Color: {self.color}")
         print(f"Genero: {self.genero}")
 
-    # Método para arrancar el auto
-    def comer(self):
-        print(f"El animal de especie: {self.especie}, de color: {self.color} esta comiendo.")
+    # Método para comer com parametro
+    def comer(self, tipoAlimento=""):
+        if tipoAlimento == "":
+            print(f"El animal de especie: {self.especie}, de color: {self.color} esta comiendo.")
+        else:
+            print(f"El animal de especie: {self.especie}, de color: {self.color} esta comiendo {tipoAlimento}.")
 
-    # Método para detener el auto
+    # Método para comer com parametro
+    def comer2(self, *args):
+        if len(args) == 0:
+            print(f"El animal de especie: {self.especie}, de color: {self.color} esta comiendo.")
+        else:
+            detalles = ", ".join(args)
+            print(f"El animal de especie: {self.especie}, de color: {self.color} esta comiendo con el siguiente detalle: {detalles}.")
+          
+    # Método para correr el animal
     def correr(self):
         print(f"El animal de especie: {self.especie}, de color: {self.color} esta corriendo.")
 
-# Crear objetos de la clase Auto
+# Crear objetos de la clase Animal
 animal1 = Animal("gato", "negro", "macho")
 animal2 = Animal("perro", "amarillo", "macho")
 
 # Usar los métodos de los objetos
 animal1.mostrar_info()
 animal1.comer()
+animal1.comer2()
 animal1.correr()
 
-print()  # Separador entre autos
+print()  # Separador entre Animales
 
 animal2.mostrar_info()
-animal2.comer()
+animal2.comer("alfalfa")
+animal2.comer2("balanceado", "30% de proteinas", "2 pm")
 animal2.correr()
